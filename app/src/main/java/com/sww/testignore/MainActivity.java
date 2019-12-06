d76d27be6529cf0b580ca60319726378efde2896
邵文文 <sww1@meitu.com>
2019-12-06 14:03:50 +0800

    测试gitIgnore

---
+++ app/src/main/java/com/sww/testignore/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sww.testignore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 参考文档：https://www.jianshu.com/p/696ca5f0ba41
+ * （1）试下哪些能上传，哪些不能上传。
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
